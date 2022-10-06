b20f7ca807cc62cf6a37f95da9472f49506b63a9
caio7700 <caiorocha113@gmail.com>
2022-10-05 21:04:00 -0300

    subindo a pasta do projeto

---
+++ src/test/java/com/project_ecommerce_cm/ECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_ecommerce_cm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
